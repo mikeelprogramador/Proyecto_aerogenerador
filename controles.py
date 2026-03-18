758485663aaff5f0527af6ac3b3bb3436bfb97ea
Mike <128723321+mikeelprogramador@users.noreply.github.com>
2026-03-18 16:35:43 -0500

    Beta 1
    Se remplazaron los hilos por after; ya que estos generaba errores al momento de crear las matrices hijas

--- controles.py
+++ controles.py
@@ -14,16 +14,18 @@ class Controladores:
         self.estado_color = ""  #   vaable para cambiar el fondo
         self.angulo = 0 #  variable predeterminada de la velocidad gular
     
-    def mostrarScalar(self,llave,etiqueta,valor):
+    def mostrarScalar(self, flag, etiqueta, valor):
         
-            #comparacion de medidas, para mostrarlas en pantalla
-        if(llave == "km/h"):
+        #comparacion de medidas, para mostrarlas en pantalla
+        if(flag == "km/h"):
             self.velocidadViento = round(float(valor),2)
             etiqueta.configure(text=f"V(viento): {self.velocidadViento} km/h")
         
-        if(llave == "°"):
+        elif(flag == "°"):
             self.temperatura = round(float(valor),0)
             etiqueta.configure(text=f"Temperatura: {self.temperatura}°")
+            
+            
             
     def anguloGiro(self):
            
@@ -33,13 +35,13 @@ class Controladores:
             if self.velocidadViento >= 25 and self.velocidadViento <= 100:
                 self.angulo = op.velocidadAngular(self.velocidadViento)
                 
-            if (self.velocidadViento >= 0 and self.velocidadViento <25) or self.velocidadViento > 100 :
+            elif (self.velocidadViento >= 0 and self.velocidadViento <25) or self.velocidadViento > 100 :
             
                 if self.angulo > 0:
                     freno = self.angulo * 0.005
                     self.angulo -= freno
                     
-                if self.angulo < 1:
+                elif self.angulo < 1:
                     self.angulo = 0
                 
         else:
@@ -47,157 +49,174 @@ class Controladores:
             if self.angulo < 1:
                 self.angulo = 0
                     
-
-
-
-        #print("Velocidad angular:",self.angulo)    #   Velocidad angular por milisegundos 
             
         return self.angulo
     
     
-
-    def electricidad(self,etiqueta_canvas = None, etiqueta = None):
-        # tiempoInical = time.perf_counter()  #   tiempo incial 
-        
-        while True:
-            
-            #   potencia de la turbina, esta puede cambiar segun el veinto o su temperatura; afectando a la potencia electrica
-            potencia_turbina = op.PotenciTurbina(self.velocidadViento,self.temperatura)
-            
-            time.sleep(1)   #   tiempo de espera x segundo
-                        
-            #   potencia eletrica segun el intervalo de tiempo inical y el tiempo de espera, con el tiemo final
-            potencia_electrica = op.PotenciaElectrica(potencia_turbina)
     
-    
-            if self.__Dv == "e":
-                #    comparacion de la velocidad del viento, para obtener la energia total  
-                if self.velocidadViento >= 25 and self.velocidadViento <= 100:
-                    #   eenrgia total generada en kwh
-                    self.energia_total = op.EnergiaTotal(tiempoInical,potencia_electrica)
-                
-                if (self.velocidadViento >= 0 and self.velocidadViento <25) or self.velocidadViento > 100 :
-                    
-                    if self.energia_total > 0:
-                        self.energia_total -= (self.energia_total * 0.1)
-                        
-                    if self.energia_total < 0.1:
-                        self.energia_total = 0
-                        
-                        
-            else:
-                self.energia_total -= (self.energia_total * 0.1) 
-                if self.energia_total < 0.1:
-                    self.energia_total = 0
-                    
-                    
-            if self.energia_total == 0:
-                tiempoInical = time.perf_counter()  #   tiempo incial
-
-            if etiqueta_canvas != None:
-                etiqueta_canvas.itemconfig(etiqueta, text=f"Energia generada: {round(float(self.energia_total),2)} kwh")
-                
-                
-                
     def cambio_direccion(self, direccion):
         self.__Dv = direccion
         
+    
+    def get_windows(self, window):
+        self.ventana = window
+        
+    def get_canvas(self, canvas):
+        self.canvas = canvas
+    
+    
+
+    def electricidad(self, etiqueta):
+        
+        ms = 200
+        
+        #   potencia de la turbina, esta puede cambiar segun el veinto o su temperatura; afectando a la potencia electrica
+        potencia_turbina = op.PotenciTurbina(self.velocidadViento,self.temperatura)
+                    
+        #   potencia eletrica segun el intervalo de tiempo inical y el tiempo de espera, con el tiemo final
+        potencia_electrica = op.PotenciaElectrica(potencia_turbina)
+        
+
+
+        if self.__Dv == "e":
+            
+            #    comparacion de la velocidad del viento, para obtener la energia total  
+            if self.velocidadViento >= 25 and self.velocidadViento <= 100:
+                #   eenrgia total generada en kwh
+                self.energia_total = op.EnergiaTotal(self.tiempoInical,potencia_electrica)
+            
+            elif (self.velocidadViento >= 0 and self.velocidadViento <25) or self.velocidadViento > 100 :
+                
+                if self.energia_total > 0:
+                    self.energia_total -= (self.energia_total * 0.03)
+                    
+                elif self.energia_total < 0.1:
+                    self.energia_total = 0
+                    
+                    
+        else:
+            self.energia_total -= (self.energia_total * 0.03) 
+            
+            if self.energia_total < 0.1:
+                self.energia_total = 0
+                
+                
+        if self.energia_total == 0:
+            self.tiempoInical = time.perf_counter()  #   tiempo incial
+                
+            
+
+        self.canvas.itemconfig(etiqueta, text=f"Energia generada: {round(float(self.energia_total),2)} kwh")
+            
+        self.ventana.after(ms, self.electricidad, etiqueta)
+                
+       
+                
         
                 
-    def cambio_color(self, etiqueta_canvas = None, etiqueta = None):
+    def cambio_color(self, etiqueta):
         
-        while True:
+        ms = 500
             
-            time.sleep(1)
+        
+        if self.energia_total > 1 and self.energia_total <= 10:
+            self.canvas.itemconfig(etiqueta, fill="red")
+        
+        elif self.energia_total > 10 and self.energia_total <= 30:
+            self.canvas.itemconfig(etiqueta, fill="yellow")
             
-            if self.energia_total > 1 and self.energia_total <= 10:
-                etiqueta_canvas.itemconfig(etiqueta, fill="red")
+        elif self.energia_total > 30 and self.energia_total <= 60:
+            self.canvas.itemconfig(etiqueta, fill="green")
             
-            elif self.energia_total > 10 and self.energia_total <= 30:
-                etiqueta_canvas.itemconfig(etiqueta, fill="yellow")
-                
-            elif self.energia_total > 30 and self.energia_total <= 60:
-                etiqueta_canvas.itemconfig(etiqueta, fill="green")
-                
-            elif self.energia_total > 60 :
-                etiqueta_canvas.itemconfig(etiqueta, fill="blue")
-                
-            elif self.energia_total < 1 :
-                etiqueta_canvas.itemconfig(etiqueta, fill="white")
+        elif self.energia_total > 60 :
+            self.canvas.itemconfig(etiqueta, fill="blue")
+            
+        elif self.energia_total < 1 :
+            self.canvas.itemconfig(etiqueta, fill="white")
+            
+        self.ventana.after(ms, self.cambio_color, etiqueta)
        
                 
                 
                 
-    def movimiento_aire(self, etiqueta_canvas, etiqueta_e, etiqueta_w ):
+    def animacion_movimiento_aire(self, etiqueta_e, etiqueta_w ):
+        
+        
+        #   sacando la longitud del viento
+        # v = int(self.velocidadViento)
+        # v = len(str(v))
+        
+        #   calculando el tiempo segun la velocidad del aire
+        #ms = int(self.velocidadViento ) / 100**int(v)
+        
+        ms = 33
+        
+        
         
         velocidad_e = 10
         velocidad_w = -10
         
-        while True:
             
-            #   extraemos la poscion y el estado de la corriente de aire por izquierda
-            x_actual_e, y_actual_e = etiqueta_canvas.coords(etiqueta_e)
-            estado_actual_e = etiqueta_canvas.itemcget(etiqueta_e, "state")
-            
-            #   extraemos la poscion y el estado de la corriente de aire por derecha
-            x_actual_w, y_actual_w = etiqueta_canvas.coords(etiqueta_w)
-            estado_actual_w = etiqueta_canvas.itemcget(etiqueta_w, "state")
-            
-            #   sacando la longitud del viento
-            v = int(self.velocidadViento)
-            v = len(str(v))
-            
-            #   calculando el tiempo segun la velocidad del aire
-            tiempo = int(self.velocidadViento ) / 100**int(v)
-            
-            #   aplicando el tiempo se puede colocar una constante o la variacion del tiempo
-            time.sleep(0.070)
-            
-            #   si la direccion es por la izquierda
-            if self.__Dv == "e":
-                
-                #   comprobar si el estado de la corriente de la derecha esta a la vista
-                if estado_actual_w != "hidden":
-                    etiqueta_canvas.coords(etiqueta_w, 1100, 300)
-                    etiqueta_canvas.itemconfig(etiqueta_w, state="hidden")
+        #   extraemos la poscion y el estado de la corriente de aire por izquierda
+        x_actual_e, y_actual_e = self.canvas.coords(etiqueta_e)
+        estado_actual_e = self.canvas.itemcget(etiqueta_e, "state")
+        
+        #   extraemos la poscion y el estado de la corriente de aire por derecha
+        x_actual_w, y_actual_w = self.canvas.coords(etiqueta_w)
+        estado_actual_w = self.canvas.itemcget(etiqueta_w, "state")
+        
 
+        
+        #   si la direccion es por la izquierda
+        if self.__Dv == "e":
+            
+            #   comprobar si el estado de la corriente de la derecha esta a la vista
+            if estado_actual_w != "hidden":
+                self.canvas.coords(etiqueta_w, 1100, 300)
+                self.canvas.itemconfig(etiqueta_w, state="hidden")
+
+            
+            #   comparacion
+            if self.velocidadViento > 0:
                 
-                if self.velocidadViento > 0:
+                if estado_actual_e != "normal":
+                    self.canvas.itemconfig(etiqueta_e, state="normal")
                     
-                    if estado_actual_e != "norma":
-                        etiqueta_canvas.itemconfig(etiqueta_e, state="normal")
-                       
-                    #   movemos la imagen de izquerda a derecha  
-                    etiqueta_canvas.move(etiqueta_e, velocidad_e, 0)
-                    
-                if x_actual_e > 1100 or self.velocidadViento < 1:
-                    
-                    if estado_actual_e != "hidden":
-                        etiqueta_canvas.coords(etiqueta_e, 0, 300)
-                        etiqueta_canvas.itemconfig(etiqueta_e, state="hidden")
-                                
-            else:
+                #   movemos la imagen de izquerda a derecha  
+                self.canvas.move(etiqueta_e, velocidad_e, 0)
                 
-                #   comprobar si el estado de la corriente de la izquiera esta a la vista
+            if x_actual_e > 1100 or self.velocidadViento < 1:
+                
                 if estado_actual_e != "hidden":
-                    etiqueta_canvas.coords(etiqueta_e, 0, 300)
-                    etiqueta_canvas.itemconfig(etiqueta_e, state="hidden")
+                    self.canvas.coords(etiqueta_e, 0, 300)
+                    self.canvas.itemconfig(etiqueta_e, state="hidden")
+                    
+                            
+        else:
+            
+            #   comprobar si el estado de la corriente de la izquiera esta a la vista
+            if estado_actual_e != "hidden":
+                self.canvas.coords(etiqueta_e, 0, 300)
+                self.canvas.itemconfig(etiqueta_e, state="hidden")
+            
+            
+            if self.velocidadViento > 0:
                 
-                
-                if self.velocidadViento > 0:
-                    if estado_actual_w != "normal":
-                        etiqueta_canvas.itemconfig(etiqueta_w, state="normal")
+                if estado_actual_w != "normal":
+                    self.canvas.itemconfig(etiqueta_w, state="normal")
                     
                 #   movemos la imagen de derecha a izquiera 
-                etiqueta_canvas.move(etiqueta_w, velocidad_w, 0)
-                
-                if x_actual_w < 0 or self.velocidadViento < 1:
-                    
-                    if estado_actual_w != "hidden":
-                        etiqueta_canvas.coords(etiqueta_w, 1100, 300)
-                        etiqueta_canvas.itemconfig(etiqueta_w, state="hidden")
-                        
+                self.canvas.move(etiqueta_w, velocidad_w, 0)
             
+            if x_actual_w < 0 or self.velocidadViento < 1:
+                
+                if estado_actual_w != "hidden":
+                    self.canvas.coords(etiqueta_w, 1100, 300)   
+                    self.canvas.itemconfig(etiqueta_w, state="hidden")
+                    
+                        
+               
+        self.ventana.after(ms, self.animacion_movimiento_aire, etiqueta_e, etiqueta_w)
                     
                 
                     
--- widgetPrincipal.py
+++ widgetPrincipal.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import Image,ImageTk
-import threading as hilo
 from controles import Controladores
 
 control = Controladores()
@@ -21,6 +20,7 @@ class ProgramPrincipal:
         #   creando en lienso o canvas
         self.canvas = Canvas(self.ventana, width=360)
         self.canvas.place(relx=0.5,rely=0.5, anchor="center", relwidth=0.75, relheight=1)
+        control.get_canvas(self.canvas)
         
         #   inicialisar imagen y tamaño
         self.imgfondo = Image.open("img/fondo.png").resize((942,1064))
@@ -30,7 +30,7 @@ class ProgramPrincipal:
         self.imgAspas = Image.open("img/aspasVertical.png").resize((621,932))
 
         
-        #   cargar la imagne a Tk
+        #   cargar la imagne con PIL y Tk
         self.imgTk_fondo = ImageTk.PhotoImage(self.imgfondo)
         self.imgTk_torre = ImageTk.PhotoImage(self.imgTorre)
         self.imgTk_corrienteAire_e = ImageTk.PhotoImage(self.imgCorrienteAire_e)
@@ -47,89 +47,115 @@ class ProgramPrincipal:
         #   estado de la imagen
         self.canvas.itemconfig(self.img_id_corrienteAire_e, state="hidden")
         self.canvas.itemconfig(self.img_id_corrienteAire_w, state="hidden")
-
         
         #   textos
         self.text_energia = self.canvas.create_text(170,50, text="Energia generada: 0.0 kwh", font=("Arial", 18))
         
-        
-        #   rectngulo
+        #   rectangulo
         self.caja_color = self.canvas.create_rectangle(650, 30, 850, 70, fill="white")
     
+    
+        """_resumen_
+        En esta sección están las animaciones de movimiento y cambio de texto 
+        """
         
-        #    calcular la electricidad todal en otro hilo o otra tarea
-        calculo_electricidad = hilo.Thread(target=control.electricidad, args=(self.canvas, self.text_energia), daemon=True)
-        calculo_electricidad.start()
+        #    animacion muestra la energia generada
+        control.electricidad(self.text_energia)
         
-        #    cambio de color segun la electricidad
-        color_electricidad = hilo.Thread(target=control.cambio_color, args=(self.canvas, self.caja_color), daemon=True)
-        color_electricidad.start()
+        #    animacion cambia de color segun  la energia generada
+        control.cambio_color(self.caja_color)
         
-        #    movimien del aire
-        movi_corriente = hilo.Thread(target=control.movimiento_aire, args=(self.canvas, 
-                                                                           self.img_id_corrienteAire_e, 
-                                                                           self.img_id_corrienteAire_w), daemon=True)
-        movi_corriente.start()
+        #    animacion movimiento del aire
+        control.animacion_movimiento_aire(self.img_id_corrienteAire_e, self.img_id_corrienteAire_w)
         
-        
-        #   Movimiento de la turbina
-        self.__programa()
+        #   animacion movimiento para las aspas
+        self.__animacion_aspas()
+
         
         
-    def __programa(self):
-        ms = 33 #   milisegundos
+    def __animacion_aspas(self):
+        """_resumen_
+        Esta función obtiene la velocidad angular y realiza la “animación /rotación” 
+        de las aspas actualizando cada frame
+        """
+        #   milisegundos
+        ms = 33 
         
         velocidadAngular = control.anguloGiro()
-        
-        dt = ms / 1000  #   tiempo entre frame
+
+        #   tiempo entre frame
+        dt = ms / 1000  
         
         self.giro += velocidadAngular * dt
         
-        rotacionImg = self.imgAspas.rotate(self.giro, expand=TRUE)  #   rotacion de la imagen
-        self.imgTk_aspas = ImageTk.PhotoImage(rotacionImg)    #   
+        #   rotacion de la imagen
+        rotacionImg = self.imgAspas.rotate(self.giro, expand=TRUE)
+        self.imgTk_aspas = ImageTk.PhotoImage(rotacionImg)
         
         #   actualizar imagen
         self.canvas.itemconfig(self.img_id_aspas, image=self.imgTk_aspas)
         
         
-        #print(self.giro)   #   el numero de giro por los milisegundos
-        
-        self.canvas.after(ms, self.__programa)   #   Ejecucion de la funcion por los milisegundos
+        self.canvas.after(ms, self.__animacion_aspas)
         
         
     def __variablesPrograma(self):
         
+        """_resumen_
+        Valor/Scalar mostrando la lectura de la temperatura
+        """
+        #   letura del aire
         self.viento = ttk.Label(self.frame, text="V(viento): 0 km/h")
-        self.viento.place(relx=0.02, rely=0.1) #posicion x & y en porcentaje
+        #   posicion x & y en porcentaje
+        self.viento.place(relx=0.02, rely=0.1)
         
+        #   valor/scalar del aire
         self.variableAire = ttk.Scale(self.frame, from_=0, to=120, orient="horizontal", 
                                        command= lambda valor: control.mostrarScalar("km/h",self.viento, valor))
         self.variableAire.place(relx=0.02, rely=0.2)
         
+        
+        """_resumen_
+        Valor/Scalar mostrando la lectura de la temperatura
+        """
+        #   lectura de la temperatura
         self.temperatura = ttk.Label(self.frame,text="Temperatura: 0°")
+        #   posicion x & y en porcentaje
         self.temperatura.place(relx=0.02, rely=0.4)
         
+        #   valor/scalar de la temperatura
         self.variableTemperatura = ttk.Scale(self.frame, from_=-20,to=100, orient="vertical", 
                                              command= lambda valor: control.mostrarScalar("°",self.temperatura,valor))
         self.variableTemperatura.place(relx=0.05, rely=0.5)
         
+        
+        """_resumen_
+        Lectura de la dirección del aire
+        """
+        #   texto
         ttk.Label(self.frame, text="Direccion del viento").place(relx=0.02, rely=0.7)
         
+        #   lectura de dirracion mediante un boton
         self.izquierda = ttk.Button(self.frame, text="Izquierda", command= lambda: control.cambio_direccion("e"))
         self.izquierda.place(relx=0.035, rely=0.75)
         
+        #   lectura de dirracion mediante un boton
         self.derecha = ttk.Button(self.frame, text="derecha", command= lambda: control.cambio_direccion("w"))
         self.derecha.place(relx=0.035, rely=0.8)
         
-        #Botones para cambiar el esenario
-        """"falta realizar la implementacion del cambio de esenario"""
         
+        """_resumen_
+        Botones para mostrar matrices hijas o los diferentes tipos de aerogeneradores 
+        """
+        #   boton
         self.boton1 = ttk.Button(self.frame, text="Boton1")
         self.boton1.place(relx=0.9, rely=0.1)
         
+        #   boton
         self.boton2 = ttk.Button(self.frame, text="Boton2")
         self.boton2.place(relx=0.9, rely=0.3)
         
+        #   boton   
         self.boton3 = ttk.Button(self.frame, text="Boton3")
         self.boton3.place(relx=0.9, rely=0.5)
         
@@ -138,6 +164,7 @@ class ProgramPrincipal:
         self.ventana.title("Simulador")
         self.ventana.geometry("1200x720")
         self.ventana.resizable(False,False) #   Desabilita el agrandamienro de la pantalla
+        control.get_windows(self.ventana)
         
 
     def EjecucionPorgrama(self):
